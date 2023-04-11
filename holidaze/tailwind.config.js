754ddd435caea15b7d0820a03805d3fd3a5cdc8c
Michael Wasilewski <michael_grimestad94@hotmail.com>
2023-04-11 11:10:44 +0200

    initial project setup

---
+++ holidaze/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{js,jsx}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#1A202C', // Primary color: for buttons, links, and calls to action.
+				secondary: '#F56565', // Secondary color: for highlighting important information or elements.
+				accent: '#48BB78', // Accent color: for error messages, warnings, or alerts.
+				neutral1: '#F7FAFC', // Neutral color 1: for backgrounds, borders, or text
+				neutral2: '#718096', // Neutral color 2: for navigation menus, footers, or other secondary elements.
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/aspect-ratio')],
+};
