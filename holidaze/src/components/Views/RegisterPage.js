8e8a1cf1b2ab01d730411962f95d76d43cddb740
Michael Wasilewski <michael_grimestad94@hotmail.com>
2023-05-08 22:36:09 +0200

    Update RegisterPage.js

--- holidaze/src/components/Views/RegisterPage.js
+++ holidaze/src/components/Views/RegisterPage.js
@@ -1,4 +1,3 @@
-import { LockClosedIcon } from '@heroicons/react/20/solid';
 import { useEffect, useRef } from 'react';
 import {
 	useSelector,
@@ -336,12 +335,6 @@ export default function AuthForm({ mode }) {
 										type="submit"
 										className="group relative flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 									>
-										<span className="absolute inset-y-0 left-0 flex items-center pl-3">
-											<LockClosedIcon
-												className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-												aria-hidden="true"
-											/>
-										</span>
 										{submitButtonText}
 									</button>
 								</div>
